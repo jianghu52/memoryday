7bcb5e9668b3a800653fc250305ef155b8555378
jianghu52 <jianghu52@gmail.com>
2013-07-27 14:29:35 +0800

    first one
    上传真他妈的费劲。

---
+++ 1/app/Controller/TestController.php
@@ -0,0 +1,14 @@
+﻿<?php
+// app/Controller/UsersController.php
+class TestController extends AppController {
+
+    var $name = 'test';
+    var $uses = Array('');
+
+    public function index() {
+        //echo "string";exit;
+        $this->set('kkkkk', '日本語テスト');
+    }
+
+        
+}
